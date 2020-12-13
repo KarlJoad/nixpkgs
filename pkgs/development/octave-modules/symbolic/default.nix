c18123bdaf72ff7bccc984123e10f4628cbdaf2b
Karl Hallsby <karl@hallsby.com>
2020-12-12 18:25:18 -0600

    octave/symbolic: init at 2.9.0

---
+++ pkgs/development/octave-modules/symbolic/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, stdenv
+, fetchTarball
+, octave
+, python
+, sympy
+, mpmath
+}:
+
+stdenv.mkDerivation rec {
+  pname = "symbolic";
+  version = "2.9.0";
+
+  src = fetchTarball {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "08l8baf4641mfcmf9bjrxi0zdh9728vwpg5nckim8jmp1b5qbxcd";
+  };
+
+  buildInputs = [
+    octave
+  ];
+
+  propagatedBuildInputs = [
+    python
+    sympy
+    mpmath
+  ];
+
+  installPhase = ''
+    mkdir -p $out/
+    cp -r $src/* $out/
+  '';
+
+  phases = [ "unpackPhase" "installPhase" ];
+
+  meta = {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ KarlJoad ];
+    description = "Adds symbolic calculation features to GNU Octave";
+  };
+}
