6842d9cc7b6875a27022f5c810274f25b671ddef
Karl Hallsby <karl@hallsby.com>
2020-12-12 18:25:37 -0600

    octave/control: init at 3.2.0
    octave/control: explicitly state dependencies

---
+++ pkgs/development/octave-modules/control/default.nix
@@ -0,0 +1,46 @@
+{stdenv
+, fetchTarball
+, octave
+, gfortran
+, autoreconfHook
+, lapack, blas, flibs
+}:
+
+stdenv.mkDerivation rec {
+  pname = "control";
+  version = "3.2.0";
+
+  src = fetchTarball {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "1l9lr3fxmjf8j900jrgcc7qffymmkmnnar4ahvaap87blgv07wcm";
+  };
+
+  buildInputs = [
+    octave
+    gfortran
+    autoreconfHook
+  ];
+
+  propagatedBuildInputs = [
+    lapack blas
+  ];
+
+  sourceRoot = "source/src";
+
+  postBuild = ''
+    mkdir -p $out/
+    cp *.oct $out/
+  '';
+
+  installPhase = ''
+    # Copy all the Octave files, with the package's functions, out.
+    cp -r $src/inst/* $out/
+  '';
+
+  meta = {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = stdenv.lib.licenses.gpl3Plus;
+    maintainers = with stdenv.pkgs.maintainers; [ KarlJoad ];
+    description = "Signal processing tools, including filtering, windowing and display functions";
+  };
+}
