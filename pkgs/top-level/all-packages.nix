782b62ab4c36a00cc4a8a5560a50056067bc4222
Karl Hallsby <karl@hallsby.com>
2020-12-16 13:12:15 -0600

    octave/io: init at 2.6.3

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -11103,6 +11103,7 @@ in
   octavePackages = callPackage ./octave-packages.nix {
     lapack = lapack;
     blas = blas;
+    jdk = jdk8;
   };
 
   ocropus = callPackage ../applications/misc/ocropus { };
