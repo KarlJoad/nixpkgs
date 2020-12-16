f2f2a8298caaf5d65a0223360ac632c16e3cf631
Karl Hallsby <karl@hallsby.com>
2020-12-15 23:56:02 -0600

    octave-modules: with fetchurl change, must change sourceRoot as well

--- pkgs/development/octave-modules/control/default.nix
+++ pkgs/development/octave-modules/control/default.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation rec {
     lapack blas
   ];
 
-  sourceRoot = "source/src";
+  sourceRoot = "${pname}-${version}/src";
 
   postBuild = ''
     mkdir -p $out/
@@ -33,14 +33,16 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
+    # Currently in $sourceRoot. End up in root of unpack.
+    cd ..
     # Copy all the Octave files, with the package's functions, out.
-    cp -r $src/inst/* $out/
+    cp -r inst/* $out
   '';
 
   postInstall = ''
     # Copy the distribution information.
     mkdir -p $out/packinfo
-    cp $src/COPYING $src/DESCRIPTION $src/INDEX $src/NEWS $out/packinfo/
+    cp COPYING DESCRIPTION INDEX NEWS $out/packinfo/
   '';
 
   meta = {
