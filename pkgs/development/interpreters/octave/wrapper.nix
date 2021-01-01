a965655f85c2ea24632cefc1d34545d9b3eb897b
Karl Hallsby <karl@hallsby.com>
2020-12-31 20:01:55 -0600

    octave-wrapper: write postBuild to symlink everything in-place
    This is somewhat delicate, as octave will need to remain unchanged,
    but the packages need to be installed somewhere. Right now, I am
    aiming for env-location/share/octave/octave_packages, like it would be
    on other GNU/Linux distributions.

--- pkgs/development/interpreters/octave/wrapper.nix
+++ pkgs/development/interpreters/octave/wrapper.nix
@@ -2,6 +2,7 @@
 , octavePackages
 , extraLibs ? []
 , extraOutputsToInstall ? []
+, postBuild ? ""
 , ignoreCollisions ? false
 }:
 
@@ -17,5 +18,47 @@ let
     inherit paths;
     inherit ignoreCollisions;
     extraOutputsToInstall = [ "out" ] ++ extraOutputsToInstall;
+
+    # During "build" we must first unlink the /share symlink to octave's /share
+    # Then, we can re-symlink the all of octave/share, except for /share/octave
+    # in env/share/octave, re-symlink everything from octave/share/octave and then
+    # perform the pkg install.
+    postBuild = ''
+      echo "Unlinking $out/share"
+      if [ -L "$out/share" ]; then
+          unlink "$out/share"
+      fi
+      echo "Making $out/share directory"
+      mkdir -p "$out/share"
+      unlink $out/*.tar.gz
+
+      echo "Octave's Share is at: ${octavePath}/share"
+      echo "Re-symlinking contents of ${octavePath}/share"
+      for f in ${octavePath}/share/*; do
+          echo "ln -s $f"
+          ln -s -t $out/share $f
+      done
+      echo "Unlinking $out/share/octave and re-symlinking with contents of ${octavePath}/share/octave"
+      if [ -L "$out/share/octave" ]; then
+         unlink "$out/share/octave"
+         mkdir -p $out/share/octave
+         echo "Unlinked!"
+         for f in ${octavePath}/share/octave/*; do
+             echo "ln -s $f"
+             ln -s -t $out/share/octave $f
+         done
+      fi
+
+      for path in ${stdenv.lib.concatStringsSep " " extraLibs}; do
+          if [ -e $path/*.tar.gz ]; then
+             echo $path
+             echo "${octaveExecutable} --eval 'pkg local_list $out/.octave_packages; pkg prefix $out/${octave.octPkgsPath}; pfx = pkg (\"prefix\"); pfx'"
+             echo "${octaveExecutable} --eval 'pkg install -nodeps -local $path/*.tar.gz'"
+             ${octaveExecutable} --eval "pkg local_list $out/.octave_packages; pkg prefix $out/${octave.octPkgsPath}; pfx = pkg (\"prefix\"); pfx; pkg install -nodeps -local $path/*.tar.gz"
+          fi
+      done
+    '' + postBuild;
+  };
+
   };
 in env
