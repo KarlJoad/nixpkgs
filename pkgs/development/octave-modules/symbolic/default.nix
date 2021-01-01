22dcbedf43c0811e39c617057dd6739a232e067e
Karl Hallsby <karl@hallsby.com>
2020-12-31 19:55:04 -0600

    octave-packages: symbolic remove now-unneeded parameters

--- pkgs/development/octave-modules/symbolic/default.nix
+++ pkgs/development/octave-modules/symbolic/default.nix
@@ -18,20 +18,11 @@ buildOctaveLibrary rec {
     sha256 = "1jr3kg9q6r4r4h3hiwq9fli6wsns73rqfzkrg25plha9195c97h8";
   };
 
-  root = "${pname}-${version}";
-  # This package has no compiled programs, so the src directory doesn't exist.
-  # Set the source root to the root of the package instead.
-  srcRoot = root;
-
   buildInputs = [
     python
     sympy
     mpmath
   ];
-
-  # Empty build phase so that tests in checkPhase are run.
-  buildPhase = "";
-  emptyBuild = true;
 
   doCheck = true;
   checkTarget = "test";
@@ -39,25 +30,6 @@ buildOctaveLibrary rec {
     gnuplot
     texinfo
   ];
-  # The Makefile has /bin/bash hardcoded. Switch to using /usr/bin/env bash.
-  # As of writing this file, octave as provided by octaveFull is broken (at least
-  # on my machine). To get around this, octave-cli still works.
-  preCheck = ''
-    sed -i s/"\/bin\/bash"/"\/usr\/bin\/env bash"/ Makefile
-    sed -i 's/octave/octave-cli/' Makefile
-    # Last test in vpa.m is a systemic problem in Symbolic. "Hide" it by making
-    # it a test we expect to fail.
-    sed -i.back s/"\%\!warning <dangerous>"$/"%\!xtest"/ inst/vpa.m
-    # Octave writes the commands run during testing to OCTAVE_HISTFILE
-    touch $OCTAVE_HISTFILE
-  '';
-
-  # Keep a copy of the octave tests detailed results in the output derivation,
-  # because someone may care.
-  postCheck = ''
-    mkdir -p $out/
-    cp fntests.log $out/${pname}-${version}-fntests.log
-  '';
 
   meta = {
     homepage = "https://octave.sourceforge.io/${pname}/index.html";
