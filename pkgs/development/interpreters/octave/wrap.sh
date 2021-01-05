f15d999e1b06225d6d2efd5227a853781e6ca23f
Karl Hallsby <karl@hallsby.com>
2021-01-05 17:16:44 -0600

    octave-wrap: add debugging output to identify issue
    The problem comes from unlinkDirReSymlinkContents NOT accepting a
    second parameter.
    This means contentsLocation is now "", meaning the expansion of ""/*
    becomes the chroot environment's root directory, which is then
    symlinked in-place

--- pkgs/development/interpreters/octave/wrap.sh
+++ pkgs/development/interpreters/octave/wrap.sh
@@ -1,10 +1,13 @@
 unlinkDirReSymlinkContents() {
     local dirToUnlink="$1"
     local contentsLocation="$2"
+    echo "Unlink = $dirToUnlink"
+    echo "Contents = $contentsLocation"
 
     unlink $dirToUnlink
     mkdir -p $dirToUnlink
     for f in $contentsLocation/*; do
+        echo "ln -s -t $dirToUnlink $f"
         ln -s -t "$dirToUnlink" "$f"
     done
 }
