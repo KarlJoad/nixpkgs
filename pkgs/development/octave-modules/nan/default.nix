b73b8a14af90927b6822d8b4778c11e9a3dbcba8
Karl Hallsby <karl@hallsby.com>
2021-01-06 11:27:31 -0600

    octave/nan: init at 3.5.2
    octave/nan: simplify meta attr set

---
+++ pkgs/development/octave-modules/nan/default.nix
@@ -0,0 +1,26 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, blas
+}:
+
+buildOctaveLibrary rec {
+  pname = "nan";
+  version = "3.5.2";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0bp8zl50f8qj5sivl88kjdswm035v4li33fiq3v1gmh0pvgbcw7a";
+  };
+
+  buildInputs = [
+    blas
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "A statistics and machine learning toolbox for data with and w/o missing values";
+  };
+}
