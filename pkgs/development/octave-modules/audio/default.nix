7aa1465180d223d51a416439d8d70a959b3ee7cf
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:40:57 -0600

    octave/audio: init at 2.0.2
    octave/audio: simplify meta attr set

---
+++ pkgs/development/octave-modules/audio/default.nix
@@ -0,0 +1,31 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, rtmidi
+}:
+
+buildOctaveLibrary rec {
+  pname = "audio";
+  version = "2.0.2";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "18lyvwmdy4b9pcv5sm7g17n3is32q23daw8fcsalkf4rj6cc6qdk";
+  };
+
+  buildInputs = [
+    rtmidi
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Audio and MIDI Toolbox for GNU Octave";
+    # Marked this way until KarlJoad gets rtmidi as a runtime dependency.
+    broken = true;
+    # Also refuses to build right now.
+    # configure: error: RTMIDI required to install octave midi package
+    # error: structure has no member 'dir'
+  };
+}
