cd404b60dfc2efb5a3ff32b8ca0fd4d66463eff0
Karl Hallsby <karl@hallsby.com>
2021-01-06 11:47:13 -0600

    octave/optim: init at 1.6.0
    octave/optim: simplify meta attr set

---
+++ pkgs/development/octave-modules/optim/default.nix
@@ -0,0 +1,29 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, struct
+, statistics
+}:
+
+buildOctaveLibrary rec {
+  pname = "optim";
+  version = "1.6.0";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "1z2h8gy99glxh5qi3r22am2vdirlbklkq0lx4r8jrx1ak7awh47r";
+  };
+
+  buildInputs = [
+    struct
+    statistics
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = with licenses; [ gpl3Plus publicDomain ];
+    # Modified BSD code seems removed
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Non-linear optimization toolkit";
+  };
+}
