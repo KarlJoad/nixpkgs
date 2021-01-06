f94b5846fec6ace8f2f91e070bdde436a8a71c89
Karl Hallsby <karl@hallsby.com>
2021-01-06 12:12:25 -0600

    octave/tsa: init at 4.6.2
    octave/tsa: simplify meta attr set

---
+++ pkgs/development/octave-modules/tsa/default.nix
@@ -0,0 +1,22 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, nan # > 3.0.0
+}:
+
+buildOctaveLibrary rec {
+  pname = "tsa";
+  version = "4.6.2";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0p2cjszzjwhp4ih3q3r67qnikgxc0fwxc12p3727jbdvzq2h10mn";
+  };
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Stochastic concepts and maximum entropy methods for time series analysis";
+  };
+}
--- pkgs/top-level/octave-packages.nix
+++ pkgs/top-level/octave-packages.nix
@@ -211,4 +211,6 @@ makeScope newScope (self:
 
     tisean = callPackage ../development/octave-modules/tisean { };
 
+    tsa = callPackage ../development/octave-modules/tsa { };
+
   })
