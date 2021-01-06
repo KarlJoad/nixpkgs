ada1a5f0e5a3ce1f413417e9d59b99c25c72f91b
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:57:52 -0600

    octave/fpl: init at 1.3.5
    octave/fpl: simplify meta attr set

---
+++ pkgs/development/octave-modules/fpl/default.nix
@@ -0,0 +1,21 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+}:
+
+buildOctaveLibrary rec {
+  pname = "fpl";
+  version = "1.3.5";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0cbpahn9flrv9ppp5xakhwh8vyyy7wzlsz22i3s93yqg9q2bh4ys";
+  };
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Collection of routines to export data produced by Finite Elements or Finite Volume Simulations in formats used by some visualization programs";
+  };
+}
