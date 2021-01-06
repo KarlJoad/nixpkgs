ea06e77590e5b552d4dfec0bf68105afbea1ac80
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:56:20 -0600

    octave/financial: init at 0.5.3
    octave/financial: simplify meta attr set

---
+++ pkgs/development/octave-modules/financial/default.nix
@@ -0,0 +1,23 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, io
+, statistics
+}:
+
+buildOctaveLibrary rec {
+  pname = "financial";
+  version = "0.5.3";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0f963yg6pwvrdk5fg7b71ny47gzy48nqxdzj2ngcfrvmb5az4vmf";
+  };
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Monte Carlo simulation, options pricing routines, financial manipulation, plotting functions and additional date manipulation tools";
+  };
+}
