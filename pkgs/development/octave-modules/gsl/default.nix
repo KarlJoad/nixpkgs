f526d8c6b6d866898d653b25fdbf0449121784a3
Karl Hallsby <karl@hallsby.com>
2021-01-06 11:10:26 -0600

    octave/gsl: init at 2.1.1
    octave/gsl: simplify meta attr set

---
+++ pkgs/development/octave-modules/gsl/default.nix
@@ -0,0 +1,26 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, gslPackage
+}:
+
+buildOctaveLibrary rec {
+  pname = "gsl";
+  version = "2.1.1";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "1lvfxbqmw8h1nlrxmvrl6j4xffmbzxfhdpxz3vrc6lg2g4jwaa6h";
+  };
+
+  buildInputs = [
+    gslPackage
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl2Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Octave bindings to the GNU Scientific Library";
+  };
+}
