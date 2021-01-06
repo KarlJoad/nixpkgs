4705ca74194437f7cc8429d60873918cec534664
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:45:33 -0600

    octave/communications: init at 1.2.2
    octave/communications: simplify meta attr set

---
+++ pkgs/development/octave-modules/communications/default.nix
@@ -0,0 +1,26 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, signal
+}:
+
+buildOctaveLibrary rec {
+  pname = "communications";
+  version = "1.2.2";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "1xay2vjyadv3ja8dmqqzm2his8s0rvidz23nq1c2yl3xh1gavyck";
+  };
+
+  buildInputs = [
+    signal
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = " Digital Communications, Error Correcting Codes (Channel Code), Source Code functions, Modulation and Galois Fields";
+  };
+}
