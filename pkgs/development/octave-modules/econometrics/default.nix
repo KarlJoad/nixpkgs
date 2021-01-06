4d4511066476a1a09d64264edb78dda2fbf82271
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:52:41 -0600

    octave/econometrics: init at 1.1.2
    octave/econometrics: simplify meta attr set

---
+++ pkgs/development/octave-modules/econometrics/default.nix
@@ -0,0 +1,26 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, optim
+}:
+
+buildOctaveLibrary rec {
+  pname = "econometrics";
+  version = "1.1.2";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "1srx78k90ycla7yisa9h593n9l8br31lsdxlspra8sxiyq0sbk72";
+  };
+
+  buildInputs = [
+    optim
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Econometrics functions including MLE and GMM based techniques";
+  };
+}
