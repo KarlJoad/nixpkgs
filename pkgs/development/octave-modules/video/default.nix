1c91946220b97f1d8363c5aa47e74691ad6a6675
Karl Hallsby <karl@hallsby.com>
2021-01-06 12:14:44 -0600

    octave/video: init at 2.0.0
    octave/video: simplify meta attr set

---
+++ pkgs/development/octave-modules/video/default.nix
@@ -0,0 +1,30 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, ffmpeg
+, libav
+}:
+
+buildOctaveLibrary rec {
+  pname = "video";
+  version = "2.0.0";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0s6j3c4dh5nsbh84s7vnd2ajcayy1gn07b4fcyrcynch3wl28mrv";
+  };
+
+  buildInputs = [
+    ffmpeg
+    libav
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = with licenses; [ gpl3Plus bsd3 ];
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Wrapper for OpenCV's CvCapture_FFMPEG and CvVideoWriter_FFMPEG";
+    # Marked this way due to configure error "error: FFmpeg libswscale, libavformat, libavcodec or libavutil not found"
+    broken = true;
+  };
+}
