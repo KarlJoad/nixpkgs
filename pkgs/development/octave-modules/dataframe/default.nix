a59df8d75585d709840947afaf49963d3b24b1c1
Karl Hallsby <karl@hallsby.com>
2021-01-06 10:49:38 -0600

    octave/dataframe: init at 1.2.0
    octave/dataframe: simplify meta attr set

---
+++ pkgs/development/octave-modules/dataframe/default.nix
@@ -0,0 +1,21 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+}:
+
+buildOctaveLibrary rec {
+  pname = "dataframe";
+  version = "1.2.0";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "10ara084gkb7d5vxv9qv7zpj8b4mm5y06nccrdy3skw5nfbb4djx";
+  };
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Data manipulation toolbox similar to R data.frame";
+  };
+}
