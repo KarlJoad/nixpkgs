1eb3405384b40a519dfe46aff2bd287a6debdfc0
Karl Hallsby <karl@hallsby.com>
2021-01-06 12:05:39 -0600

    octave/sparsersb: init at 1.0.8
    octave/sparsersb: simplify meta attr set

---
+++ pkgs/development/octave-modules/sparsersb/default.nix
@@ -0,0 +1,28 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, librsb
+}:
+
+buildOctaveLibrary rec {
+  pname = "sparsersb";
+  version = "1.0.8";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0nl7qppa1cm51188hqhbfswlih9hmy1yz7v0f5i07z0g0kbd62xw";
+  };
+
+  buildInputs = [
+    librsb
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ KarlJoad ];
+    description = "Interface to the librsb package implementing the RSB sparse matrix format for fast shared-memory sparse matrix computations";
+    # Mark this way until KarlJoad builds librsb specifically for this package.
+    broken = true;
+  };
+}
