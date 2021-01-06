9715c16bf75cddcacf0ab673f5b1fe718756d0ea
Karl Hallsby <karl@hallsby.com>
2021-01-06 00:58:32 -0600

    octave/ltfat: init at 2.3.1
    octave/ltfat: add meta.name for a better full name of package
    
    octave/ltfat: mark as broken for now
    
    For the initial PR I am building with the octave packages already
    defined, I will leave these as broken, so they can't be used, but they
    are present for people to see and comment on.

---
+++ pkgs/development/octave-modules/ltfat/default.nix
@@ -0,0 +1,54 @@
+{ buildOctaveLibrary
+, stdenv
+, fetchurl
+, fftw
+, fftwSinglePrec
+, fftwFloat
+, fftwLongDouble
+, lapack
+, blas
+, portaudio
+, jre
+}:
+
+buildOctaveLibrary rec {
+  pname = "ltfat";
+  version = "2.3.1";
+
+  src = fetchurl {
+    url = "https://octave.sourceforge.io/download.php?package=${pname}-${version}.tar.gz";
+    sha256 = "0gghh5a4w649ff776wvidfvqas87m0n7rqs960pid1d11bnyqqrh";
+  };
+
+  buildInputs = [
+    fftw
+    fftwSinglePrec
+    fftwFloat
+    fftwLongDouble
+    lapack
+    blas
+    portaudio
+    jre
+  ];
+
+  meta = {
+    name = "The Large Time-Frequency Analysis Toolbox";
+    homepage = "https://octave.sourceforge.io/${pname}/index.html";
+    license = stdenv.lib.licenses.gpl3Plus;
+    maintainers = with stdenv.pkgs.maintainers; [ KarlJoad ];
+    description = "Toolbox for working with time-frequency analysis, wavelets and signal processing";
+    longDescription = ''
+      The Large Time/Frequency Analysis Toolbox (LTFAT) is a Matlab/Octave
+      toolbox for working with time-frequency analysis, wavelets and signal
+      processing. It is intended both as an educational and a computational
+      tool. The toolbox provides a large number of linear transforms including
+      Gabor and wavelet transforms along with routines for constructing windows
+      (filter prototypes) and routines for manipulating coefficients.
+    '';
+    broken = true;
+    # During a simple build, I received the following error while building documentation.
+    # error: parse error near line 153 of file /nix/store/hash-octave-6.1.0-env/share/octave/octave_packages/ltfat-2.3.1/nonstatgab/nsdgt.m
+    # syntax error times,f(win_range,:),g{ii}(idx));
+    # Marking as broken until I resolve it.
+  };
+}
