faa77b7ee7b1e9bccb9ca6f8d8ddc2ea0bf639a3
Karl Hallsby <karl@hallsby.com>
2021-01-07 12:35:19 -0600

    octave/io: identified missing runtime dependency during testing
    The io package expects unzip to be present in the environment.
    This is because the major spreadsheet formats (.xlsx, .xls, .ods) are
    zipped formats, and to read the contents, they must be unzipped.
    
    Marking as broken because major functionality of io is currently
    unavailable.

--- pkgs/development/octave-modules/io/default.nix
+++ pkgs/development/octave-modules/io/default.nix
@@ -3,6 +3,7 @@
 , fetchurl
 , enableJava ? true
 , jdk ? null
+, unzip
 }:
 
 buildOctaveLibrary rec {
@@ -14,12 +15,17 @@ buildOctaveLibrary rec {
     sha256 = "044y8lfp93fx0592mv6x2ss0nvjkjgvlci3c3ahav76pk1j3rikb";
   };
 
-  buildInputs = stdenv.lib.optional enableJava jdk;
+  buildInputs = [
+    (stdenv.lib.optional enableJava jdk)
+    unzip
+  ];
 
   meta = with stdenv.lib; {
     homepage = "https://octave.sourceforge.io/${pname}/index.html";
     license = with licenses; [ gpl3Plus bsd2 ];
     maintainers = with maintainers; [ KarlJoad ];
     description = "Input/Output in external formats";
+    # Marked this way until KarlJoad makes unzip a run-time dependency
+    broken = true;
   };
 }
