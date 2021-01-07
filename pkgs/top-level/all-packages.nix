e1c449e33c70597a3f9ab89cd03640f2adf494de
Karl Hallsby <karl@hallsby.com>
2021-01-06 21:53:46 -0600

    octave-packages: use recurseIntoAttrs in all-packages.nix
    This way, there is no messy passing of packages to define
    octavePackages in all-packages.nix.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10735,11 +10735,7 @@ in
     overridePlatforms = ["x86_64-linux" "x86_64-darwin"];
   };
 
-  octavePackages = callPackage ./octave-packages.nix {
-    lapack = lapack;
-    blas = blas;
-    jdk = jdk8;
-  };
+  octavePackages = recurseIntoAttrs octave.pkgs;
 
   ocropus = callPackage ../applications/misc/ocropus { };
 
