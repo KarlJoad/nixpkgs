b9b909b1767ac2fd263ff78e48c41c900dafb46f
Karl Hallsby <karl@hallsby.com>
2021-01-10 00:30:33 -0600

    octave: remove second evaluation of pkgs inside octave's build
    In response to:
    https://github.com/NixOS/nixpkgs/pull/108562#discussion_r554432190

--- pkgs/development/interpreters/octave/default.nix
+++ pkgs/development/interpreters/octave/default.nix
@@ -61,22 +61,20 @@
 }:
 
 let
-  self = pkgs.octave;
-
   # Not always evaluated
   blas' = if use64BitIdx then
     blas.override {
       isILP64 = true;
     }
-  else
-    blas
+          else
+            blas
   ;
   lapack' = if use64BitIdx then
     lapack.override {
       isILP64 = true;
     }
-  else
-    lapack
+            else
+              lapack
   ;
   qrupdate' = qrupdate.override {
     # If use64BitIdx is false, this override doesn't evaluate to a new
@@ -94,11 +92,12 @@ let
       blas = blas';
       lapack = lapack';
     }
-  else
-    null
+                 else
+                   null
   ;
 
   octavePackages = import ../../../top-level/octave-packages.nix {
+    inherit pkgs;
     inherit (pkgs) lib stdenv fetchurl newScope;
     octave = self;
   };
@@ -108,132 +107,132 @@ let
     inherit (pkgs) makeSetupHook makeWrapper;
   };
 
-in mkDerivation rec {
-  version = "6.1.0";
-  pname = "octave";
+  self = mkDerivation rec {
+    version = "6.1.0";
+    pname = "octave";
 
-  src = fetchurl {
-    url = "mirror://gnu/octave/${pname}-${version}.tar.gz";
-    sha256 = "0mqa1g3fq0q45mqc0didr8vl6bk7jzj6gjsf1522qqjq2r04xwvg";
-  };
+    src = fetchurl {
+      url = "mirror://gnu/octave/${pname}-${version}.tar.gz";
+      sha256 = "0mqa1g3fq0q45mqc0didr8vl6bk7jzj6gjsf1522qqjq2r04xwvg";
+    };
 
-  buildInputs = [
-    readline
-    ncurses
-    perl
-    flex
-    qhull
-    graphicsmagick
-    pcre
-    fltk
-    zlib
-    curl
-    blas'
-    lapack'
-    libsndfile
-    fftw
-    fftwSinglePrec
-    portaudio
-    qrupdate'
-    arpack'
-    libwebp
-    gl2ps
-  ]
-  ++ stdenv.lib.optionals enableQt [
-    qtbase
-    qtsvg
-    qscintilla
-  ]
-  ++ stdenv.lib.optionals (ghostscript != null) [ ghostscript ]
-  ++ stdenv.lib.optionals (hdf5 != null) [ hdf5 ]
-  ++ stdenv.lib.optionals (glpk != null) [ glpk ]
-  ++ stdenv.lib.optionals (suitesparse != null) [ suitesparse' ]
-  ++ stdenv.lib.optionals (enableJava) [ jdk ]
-  ++ stdenv.lib.optionals (sundials != null) [ sundials ]
-  ++ stdenv.lib.optionals (gnuplot != null) [ gnuplot ]
-  ++ stdenv.lib.optionals (python != null) [ python ]
-  ++ stdenv.lib.optionals (!stdenv.isDarwin) [ libGL libGLU libX11 ]
-  ++ stdenv.lib.optionals stdenv.isDarwin [
-    libiconv
-    darwin.apple_sdk.frameworks.Accelerate
-    darwin.apple_sdk.frameworks.Cocoa
-  ]
-  ;
-  nativeBuildInputs = [
-    pkgconfig
-    gfortran
-    # Listed here as well because it's outputs are split
-    fftw
-    fftwSinglePrec
-    texinfo
-  ]
-  ++ stdenv.lib.optionals (sundials != null) [ sundials ]
-  ++ stdenv.lib.optionals enableJIT [ llvm ]
-  ++ stdenv.lib.optionals enableQt [
-    qtscript
-    qttools
-  ]
-  ;
+    buildInputs = [
+      readline
+      ncurses
+      perl
+      flex
+      qhull
+      graphicsmagick
+      pcre
+      fltk
+      zlib
+      curl
+      blas'
+      lapack'
+      libsndfile
+      fftw
+      fftwSinglePrec
+      portaudio
+      qrupdate'
+      arpack'
+      libwebp
+      gl2ps
+    ]
+    ++ stdenv.lib.optionals enableQt [
+      qtbase
+      qtsvg
+      qscintilla
+    ]
+    ++ stdenv.lib.optionals (ghostscript != null) [ ghostscript ]
+    ++ stdenv.lib.optionals (hdf5 != null) [ hdf5 ]
+    ++ stdenv.lib.optionals (glpk != null) [ glpk ]
+    ++ stdenv.lib.optionals (suitesparse != null) [ suitesparse' ]
+    ++ stdenv.lib.optionals (enableJava) [ jdk ]
+    ++ stdenv.lib.optionals (sundials != null) [ sundials ]
+    ++ stdenv.lib.optionals (gnuplot != null) [ gnuplot ]
+    ++ stdenv.lib.optionals (python != null) [ python ]
+    ++ stdenv.lib.optionals (!stdenv.isDarwin) [ libGL libGLU libX11 ]
+    ++ stdenv.lib.optionals stdenv.isDarwin [
+      libiconv
+      darwin.apple_sdk.frameworks.Accelerate
+      darwin.apple_sdk.frameworks.Cocoa
+    ]
+    ;
+    nativeBuildInputs = [
+      pkgconfig
+      gfortran
+      # Listed here as well because it's outputs are split
+      fftw
+      fftwSinglePrec
+      texinfo
+    ]
+    ++ stdenv.lib.optionals (sundials != null) [ sundials ]
+    ++ stdenv.lib.optionals enableJIT [ llvm ]
+    ++ stdenv.lib.optionals enableQt [
+      qtscript
+      qttools
+    ]
+    ;
 
-  doCheck = !stdenv.isDarwin;
+    doCheck = !stdenv.isDarwin;
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  # See https://savannah.gnu.org/bugs/?50339
-  F77_INTEGER_8_FLAG = if use64BitIdx then "-fdefault-integer-8" else "";
+    # See https://savannah.gnu.org/bugs/?50339
+    F77_INTEGER_8_FLAG = if use64BitIdx then "-fdefault-integer-8" else "";
 
-  configureFlags = [
-    "--with-blas=blas"
-    "--with-lapack=lapack"
-    (if use64BitIdx then "--enable-64" else "--disable-64")
-  ]
+    configureFlags = [
+      "--with-blas=blas"
+      "--with-lapack=lapack"
+      (if use64BitIdx then "--enable-64" else "--disable-64")
+    ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ "--enable-link-all-dependencies" ]
     ++ stdenv.lib.optionals enableReadline [ "--enable-readline" ]
     ++ stdenv.lib.optionals stdenv.isDarwin [ "--with-x=no" ]
     ++ stdenv.lib.optionals enableQt [ "--with-qt=5" ]
     ++ stdenv.lib.optionals enableJIT [ "--enable-jit" ]
-  ;
+    ;
 
-  # Keep a copy of the octave tests detailed results in the output
-  # derivation, because someone may care
-  postInstall = ''
+    # Keep a copy of the octave tests detailed results in the output
+    # derivation, because someone may care
+    postInstall = ''
     cp test/fntests.log $out/share/octave/${pname}-${version}-fntests.log || true
   '';
 
-  passthru = rec {
-    octave = self;
-    sitePath = "share/octave/${version}/site";
-    sitePackages = "share/octave/octave_packages";
-    blas = blas';
-    lapack = lapack';
-    qrupdate = qrupdate';
-    arpack = arpack';
-    suitesparse = suitesparse';
-    inherit fftw fftwSinglePrec;
-    inherit portaudio;
-    inherit jdk;
-    inherit python;
-    inherit enableQt enableJIT enableReadline enableJava;
-    buildEnv = callPackage ./wrapper.nix {
+    passthru = rec {
       octave = self;
-      inherit octavePackages;
-      wrapOctave = wrapOctave;
+      sitePath = "share/octave/${version}/site";
+      sitePackages = "share/octave/octave_packages";
+      blas = blas';
+      lapack = lapack';
+      qrupdate = qrupdate';
+      arpack = arpack';
+      suitesparse = suitesparse';
+      inherit fftw fftwSinglePrec;
+      inherit portaudio;
+      inherit jdk;
+      inherit python;
+      inherit enableQt enableJIT enableReadline enableJava;
+      buildEnv = callPackage ./wrapper.nix {
+        octave = self;
+        inherit octavePackages wrapOctave;
+      };
+      withPackages = import ./with-packages.nix { inherit buildEnv octavePackages; };
+      pkgs = octavePackages;
+      interpreter = "${self}/bin/octave";
+      octPkgsPath = "share/octave/octave_packages";
     };
-    withPackages = import ./with-packages.nix { inherit buildEnv octavePackages; };
-    pkgs = octavePackages;
-    interpreter = "${self}/bin/octave";
-    octPkgsPath = "share/octave/octave_packages";
-  };
 
-  meta = {
-    homepage = "https://www.gnu.org/software/octave/";
-    license = stdenv.lib.licenses.gpl3Plus;
-    maintainers = with stdenv.lib.maintainers; [ raskin doronbehar ];
-    description = "Scientific Pragramming Language";
-    # https://savannah.gnu.org/bugs/?func=detailitem&item_id=56425 is the best attempt to fix JIT
-    broken = enableJIT;
-    platforms = if overridePlatforms == null then
-      (with stdenv.lib; platforms.linux ++ platforms.darwin)
-    else overridePlatforms;
+    meta = {
+      homepage = "https://www.gnu.org/software/octave/";
+      license = stdenv.lib.licenses.gpl3Plus;
+      maintainers = with stdenv.lib.maintainers; [ raskin doronbehar ];
+      description = "Scientific Pragramming Language";
+      # https://savannah.gnu.org/bugs/?func=detailitem&item_id=56425 is the best attempt to fix JIT
+      broken = enableJIT;
+      platforms = if overridePlatforms == null then
+        (with stdenv.lib; platforms.linux ++ platforms.darwin)
+                  else overridePlatforms;
+    };
   };
-}
+in self
