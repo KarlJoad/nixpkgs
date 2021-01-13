77bf263daa2be7f682a6b52456e87bee28e2363e
Karl Hallsby <karl@hallsby.com>
2021-01-13 00:11:08 -0600

    octave-wrap.sh: add createOctavePackagesPath bash function

--- pkgs/development/interpreters/octave/wrap.sh
+++ pkgs/development/interpreters/octave/wrap.sh
@@ -10,3 +10,19 @@ unlinkDirReSymlinkContents() {
     done
 }
 
+createOctavePackagesPath() {
+    local desiredOut=$1
+    local origin=$2
+
+    if [ -L "$out/share" ]; then
+	unlinkDirReSymlinkContents "$desiredOut" "$origin" "share"
+    fi
+    if [ -L "$out/share/octave" ]; then
+	unlinkDirReSymlinkContents "$desiredOut" "$origin" "share/octave"
+    fi
+
+    # Now that octave_packages has a path rather than symlinks, create the
+    # octave_packages directory for installed packages.
+    mkdir -p "$desiredOut/share/octave/octave_packages"
+}
+
