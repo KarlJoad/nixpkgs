2bcbb91af387ffea4fe82441da43de8a096f0077
Karl Hallsby <karl@hallsby.com>
2021-01-13 00:09:44 -0600

    wrap-octave: improve documentation comments

--- pkgs/development/interpreters/octave/wrap-octave.nix
+++ pkgs/development/interpreters/octave/wrap-octave.nix
@@ -7,7 +7,9 @@
 with lib;
 
 # Defined in trivial-builders.nix
-# Imported as wrapOctave in octave/default.nix and passed to octave's buildEnv as nativeBuildInput
+# Imported as wrapOctave in octave/default.nix and passed to octave's buildEnv
+# as nativeBuildInput
+# Each of the substitutions is available in the wrap.sh script as @thingSubstituted@
 makeSetupHook {
   deps = makeWrapper;
   substitutions.executable = octave.interpreter;
