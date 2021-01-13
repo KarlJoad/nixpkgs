5c9935e310021b6902b625be37354b26a4276f4e
Karl Hallsby <karl@hallsby.com>
2021-01-13 00:11:28 -0600

    octave-wrap.sh: add addPkgLocalList bash function

--- pkgs/development/interpreters/octave/wrap.sh
+++ pkgs/development/interpreters/octave/wrap.sh
@@ -26,3 +26,21 @@ createOctavePackagesPath() {
     mkdir -p "$desiredOut/share/octave/octave_packages"
 }
 
+addPkgLocalList() {
+    local desiredOut=$1
+    local origin=$2
+    local octaveSite="share/octave/site"
+    local octaveSiteM="$octaveSite/m"
+    local octaveSiteStartup="$octaveSiteM/startup"
+    local siteOctavercStartup="$octaveSiteStartup/octaverc"
+
+    unlinkDirReSymlinkContents "$desiredOut" "$origin" "$octaveSite"
+    unlinkDirReSymlinkContents "$desiredOut" "$origin" "$octaveSiteM"
+    unlinkDirReSymlinkContents "$desiredOut" "$origin" "$octaveSiteStartup"
+
+    unlink "$out/$siteOctavercStartup"
+    cp "$origin/$siteOctavercStartup" "$desiredOut/$siteOctavercStartup"
+    chmod u+w "$desiredOut/$siteOctavercStartup"
+    echo "pkg local_list $out/.octave_packages" >> "$desiredOut/$siteOctavercStartup"
+}
+
