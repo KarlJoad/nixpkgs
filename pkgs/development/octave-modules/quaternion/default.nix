c295a00cfa2029aaba808726941250e62e1b4109
Karl Hallsby <karl@hallsby.com>
2021-01-16 21:01:40 -0600

    octave.pkgs.quaternion: remove broken label
    Use a `sed` command to fix syntactic/API errors in the source files,
    to allow compilation.
    
    This also allows the package to pass all its tests.
    
    octave/quaternion: remove unneeded dontPatch = false;

--- pkgs/development/octave-modules/quaternion/default.nix
+++ pkgs/development/octave-modules/quaternion/default.nix
@@ -12,12 +12,18 @@ buildOctavePackage rec {
     sha256 = "040ncksf0xz32qmi4484xs3q01nappxrsvwwa60g04yjy7c4sbac";
   };
 
+  # Octave replaced many of the is_thing_type check function with isthing.
+  # The patch changes the occurrences of the old functions.
+  patchPhase = ''
+    sed -i s/is_numeric_type/isnumeric/g src/*.cc
+    sed -i s/is_real_type/isreal/g src/*.cc
+    sed -i s/is_bool_type/islogical/g src/*.cc
+  '';
+
   meta = with lib; {
     homepage = "https://octave.sourceforge.io/quaternion/index.html";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ KarlJoad ];
     description = "Quaternion package for GNU Octave, includes a quaternion class with overloaded operators";
-    # Marked this way because of build error "s_real_array.cc:50:34: error: 'const class octave_value' has no member named 'is_bool_type'"
-    broken = true;
   };
 }
