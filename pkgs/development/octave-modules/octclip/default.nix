9c8e1e8644b09d2e7a3d1f9e7cfde112e30234c1
Karl Hallsby <karl@hallsby.com>
2021-01-16 20:59:30 -0600

    octave.pkgs.octclip: remove broken label
    Use a `sed` command to fix a syntactic error in the source files, to
    allow compilation.
    
    This also allows the package to pass all its tests, although there are
    only 6 tests in the whole package.
    
    octave/octclip: remove unneeded dontPatch = false;

--- pkgs/development/octave-modules/octclip/default.nix
+++ pkgs/development/octave-modules/octclip/default.nix
@@ -12,13 +12,18 @@ buildOctavePackage rec {
     sha256 = "05ijh3izgfaan84n6zp690nap9vnz0zicjd0cgvd1c6askm7vxql";
   };
 
+  # The only compilation problem is that no formatting specifier was provided
+  # for the error function. Because errorText is a string, I provide such a
+  # formatting specifier.
+  patchPhase = ''
+    sed -i s/"error(errorText)"/"error(\"%s\", errorText)"/g src/*.cc
+  '';
+
   meta = with lib; {
     name = "GNU Octave Clipping Polygons Tool";
     homepage = "https://octave.sourceforge.io/octclip/index.html";
     license = with licenses; [ gpl3Plus ]; # modified BSD?
     maintainers = with maintainers; [ KarlJoad ];
     description = "Perform boolean operations with polygons using the Greiner-Hormann algorithm";
-    # Marked this way because of build error "_oc_polybool.cc:220:19: error: format not a string literal and no format arguments [-Werror=format-security]"
-    broken = true;
   };
 }
