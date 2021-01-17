90ea8f5b5ea2f73666bc96a4da1c79b7dd1de1c6
Karl Hallsby <karl@hallsby.com>
2021-01-16 20:56:45 -0600

    octave.pkgs.nurbs: remove broken label
    This was managed through a set of `sed` runs that correct
    syntactic/API errors.
    
    This passes most tests, until it reaches a point where an input string
    is invalid UTF-8 input.
    
    octave/nurbs: remove unneeded dontPatch = false;

--- pkgs/development/octave-modules/nurbs/default.nix
+++ pkgs/development/octave-modules/nurbs/default.nix
@@ -12,12 +12,19 @@ buildOctavePackage rec {
     sha256 = "0zkyldm63pc3pcal3yvj6af24cvpjvv9qfhf0ihhwcsh4w3yggyv";
   };
 
+  # Has been fixed in more recent commits, but has not been pushed out as a
+  # new version yet.
+  # The sed changes allow nurbs to compile.
+  patchPhase = ''
+    sed -i s/feval/octave::feval/g src/*.cc
+    sed -i s/is_real_type/isreal/g src/*.cc
+    sed -i s/is_cell/iscell/g src/*.cc
+  '';
+
   meta = with lib; {
     homepage = "https://octave.sourceforge.io/nurbs/index.html";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ KarlJoad ];
     description = "Collection of routines for the creation, and manipulation of Non-Uniform Rational B-Splines (NURBS), based on the NURBS toolbox by Mark Spink";
-    # Marked this way because of build error "error: structure has no member 'dir'"
-    broken = true;
   };
 }
