e1ded63e3de8e4176d8b2b23455534f151f2a531
Karl Hallsby <karl@hallsby.com>
2021-01-20 20:48:22 -0600

    octave.pkgs.strings: remove broken label

--- pkgs/development/octave-modules/strings/default.nix
+++ pkgs/development/octave-modules/strings/default.nix
@@ -17,13 +17,21 @@ buildOctavePackage rec {
     pcre
   ];
 
+  # The gripes library no longer exists.
+  # https://build.opensuse.org/package/view_file/openSUSE:Backports:SLE-15-SP3/octave-forge-strings/octave-forge-strings.spec
+  # toascii is a deprecated function. Has been fixed in recent commits, but has
+  # not been released yet.
+  # https://sourceforge.net/p/octave/strings/ci/2db1dbb75557eef94605cb4ac682783ab78ac8d8/
+  patchPhase = ''
+    sed -i -s -e 's/gripes.h/errwarn.h/' -e 's/gripe_/err_/g' src/*.cc
+    sed -i s/toascii/double/g inst/*.m
+  '';
+
   meta = with lib; {
     homepage = "https://octave.sourceforge.io/strings/index.html";
     license = licenses.gpl3Plus;
     # Claims to have a freebsd license, but I found none.
     maintainers = with maintainers; [ KarlJoad ];
     description = "Additional functions for manipulation and analysis of strings";
-    # Marked this way because of build error "error: 'gripe_wrong_type_arg' was not declared in this scope"
-    broken = true;
   };
 }
